868f7a391d5d327a7ddb9ff35cdda1b1bdb99a91
ATUL GHODMARE <140696060+atulghodmare777@users.noreply.github.com>
2025-02-06 20:51:03 +0530

    Create sorted_array.py

---
+++ sorted_array.py
@@ -0,0 +1,17 @@
+def intersection_sorted_arrays(array1,array2):
+  i,j=0,0
+  result=[0]
+  while i < len(array1) and j < len(array2);
+    if array1[i] < array2[j];
+      i+=1
+elif array1[i] > array2[j];
+      j+=1
+else:
+  if not result or result [-1]! = array1[i]:
+    result.append(array1[i])
+  i+=1
+  j+=1
+  return result
+array1=[1,2,2,3,4,5]
+array2=[2,2,5,6]
+print (intersection_sorted_arrays(array1,array2))
