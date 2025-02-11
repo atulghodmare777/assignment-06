d0de6f95bbf3601c8c28c1c28d5604cac3110dc3
ATUL GHODMARE <140696060+atulghodmare777@users.noreply.github.com>
2025-02-12 00:46:31 +0530

    Update text_manupulation.sh

--- text_manupulation.sh
+++ text_manupulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 input_file="input.txt"
-cat $input_file | tr -cd '[:alpha"] [:space:]' |
-tr '[:upper:]' '[:lower:]' | \
-awk '{for (i=1 <=NF); i++) word [$i] ++} |
-{ for (w in-word) print word [w] [w]} | \
+
+# Process the text: remove non-alphabetic characters, convert to lowercase, count word occurrences, and find top 5 words
+cat "$input_file" | tr -cd '[:alpha:][:space:]' | tr '[:upper:]' '[:lower:]' |
+awk '{for (i=1; i<=NF; i++) word[$i]++} END {for (w in word) print word[w], w}' |
 sort -nr | head -n 5
